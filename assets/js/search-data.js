473def5e14ac3b425797d1d77928005f678b7c2d
JunnoY <95646150+JunnoY@users.noreply.github.com>
2026-01-31 19:27:27 +0000

    Deploying to gh-pages from @ JunnoY/junnoy.github.io@34298e8dac7a5df48875a4d76e270e84502fe462 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,9 +16,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "projects-robot-learning-for-crossing-a-red-line",
-          title: 'Robot Learning for Crossing a Red Line',
-          description: "Learn a policy (in `robot.py`) that crosses the red line efficiently under a limited training budget.",
+        },{id: "projects-robot-learning-for-navigation",
+          title: 'Robot learning for Navigation',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/IC_robot_learning/";
             },},{id: "projects-real-time-vision-based-hand-tracking-for-robot-teleoperation-and-learning",
